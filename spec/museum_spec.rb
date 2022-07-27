922e34bc0e483b5b37d19bdaa1bcada67107ac3d
Sid Mann <sjmann2@outlook.com>
2022-07-27 10:20:02 -0500

    finish test for museum spec

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,6 +8,8 @@ describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   it 'exists' do
@@ -28,5 +30,14 @@ describe Museum do
     @dmns.add_exhibit(@imax)
 
     expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end
+
+  it 'can recommend exhibits based on patrons interests' do
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
 end
