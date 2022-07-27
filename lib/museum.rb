ce5b3d9f0685a37ebb49702ddbf7a180a97e090a
Sid Mann <sjmann2@outlook.com>
2022-07-27 11:19:02 -0500

    make test pass

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -21,5 +23,24 @@ class Museum
       end
     end
     recommended_exhibits
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    patrons_by_exhibit_interest = Hash.new { |h, k| h[k] = []}
+
+    @exhibits.each do |exhibit|
+      @patrons.each do |patron|
+        if patron.interests.include?(exhibit.name)
+        patrons_by_exhibit_interest[exhibit] << patron
+        elsif patrons_by_exhibit_interest[exhibit].nil?
+          patrons_by_exhibit_interest = []
+        end
+      end
+    end
+  patrons_by_exhibit_interest
   end
 end
